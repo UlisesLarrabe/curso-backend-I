5dabc800f364fb3f8bf27123fed9ac22f79ea724
Ulises <larrabeulises@gmail.com>
2024-08-21 15:51:52 -0300

    Add session conf

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import { Server } from "socket.io";
 import viewsRouter from "./routes/views.router.js";
 import "./database.js";
 import ProductManager from "./dao/db/product-manager-db.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import sessionsRouter from "./routes/sessions.router.js";
 
 const app = express();
 app.use(express.json());
@@ -18,10 +21,26 @@ app.use(express.static("src/public"));
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "src/views");
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://Alejandrou:ulises2004@cluster0.pgv74zu.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0",
+      ttl: 60 * 10,
+    }),
+    secret: "gokueselmejorprotagonista",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000,
+    },
+  })
+);
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
+app.use("/api/sessions", sessionsRouter);
 
 const httpServer = app.listen(8080, () => {
   console.log("Listening in http://localhost:8080");
