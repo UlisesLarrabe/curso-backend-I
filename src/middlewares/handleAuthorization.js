645246abd0cca59029493ab1f4251c8513a5234e
Ulises <larrabeulises@gmail.com>
2024-09-09 10:36:02 -0300

    Add handleAutheticate function

---
+++ src/middlewares/handleAuthorization.js
@@ -0,0 +1,9 @@
+export const handleAuth = (role) => {
+  return (req, res, next) => {
+    if (req.user && req.user.role === role) {
+      next();
+    } else {
+      res.status(401).send("Unauthorized");
+    }
+  };
+};
