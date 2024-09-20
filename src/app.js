13d4f0cc87ca1196dfe923c47ace5422596ea616
Ulises <larrabeulises@gmail.com>
2024-09-20 12:15:56 -0300

    Modify new product manager

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from "express";
-import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import exphbs from "express-handlebars";
 import { Server } from "socket.io";
 import viewsRouter from "./routes/views.router.js";
 import "./database.js";
-import ProductManager from "./dao/db/product-manager-db.js";
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import initializePassport from "./strategies/current.js";
 import routes from "./routes/index.js";
+import ProductService from "./services/product.services.js";
 
 const app = express();
 app.use(express.json());
@@ -26,7 +25,6 @@ app.set("views", "src/views");
 initializePassport();
 app.use(passport.initialize());
 app.use(cookieParser("palabrasupersecreta"));
-app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 app.use("/", routes);
@@ -35,20 +33,20 @@ const httpServer = app.listen(8080, () => {
   console.log("Listening in http://localhost:8080");
 });
 
-const productManager = new ProductManager();
+const productManager = new ProductService();
 
 const io = new Server(httpServer);
 io.on("connection", async (socket) => {
-  const products = await productManager.getProducts();
+  const products = await productManager.getAllProducts();
   socket.emit("products", products.docs);
   socket.on("deleteProduct", async (id) => {
-    await productManager.deleteProduct(id);
-    const products = await productManager.getProducts();
+    await productManager.delete(id);
+    const products = await productManager.getAllProducts();
     io.sockets.emit("products", products.docs);
   });
   socket.on("addProduct", async (product) => {
-    await productManager.addProduct(product);
-    const products = await productManager.getProducts();
+    await productManager.createProduct(product);
+    const products = await productManager.getAllProducts();
     io.sockets.emit("products", products.docs);
   });
 });
