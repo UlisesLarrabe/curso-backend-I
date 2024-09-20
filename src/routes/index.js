129cd2f59294e756d5de666b3ff42aa71dd6f079
Ulises <larrabeulises@gmail.com>
2024-09-20 12:16:08 -0300

    Add product router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import UserRouterCustom from "./user.router.js";
+import ProductsRouterCustom from "./products.router.js";
 
 const router = Router();
 
 const userRouter = new UserRouterCustom();
+const productsRouter = new ProductsRouterCustom();
 
 router.use("/api/users", userRouter.getRouter());
+router.use("/api/products", productsRouter.getRouter());
 
 export default router;
